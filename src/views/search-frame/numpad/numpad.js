02f4b0ccb320263b3977b10c3dc39940769c67b9
Alexsander Alves <alvesalexsander@live.com>
2022-01-20 23:42:47 -0300

    dando forma ao numpad com grid layout

--- src/views/search-frame/numpad/numpad.js
+++ src/views/search-frame/numpad/numpad.js
@@ -3,7 +3,16 @@ import './numpad.scss';
 export function NumPad(props) {
   return (
     <div className='numpad'>
-      NUMPAD
+      <button class="key">1</button>
+      <button class="key">2</button>
+      <button class="key">3</button>
+      <button class="key">4</button>
+      <button class="key">5</button>
+      <button class="key">6</button>
+      <button class="key">7</button>
+      <button class="key">8</button>
+      <button class="key">9</button>
+      <button class="key key-centered">0</button>
     </div>
   )
 }
